cc7cb94e3238b984de547cf69f536062497ea4d3
Josue <josuepereztorr@gmail.com>
2022-05-09 22:49:21 -0500

    - Configured application with Dependency Injection to achieve IoC (Inversion of Control)

--- NorthwindConsoleApplication/Program.cs
+++ NorthwindConsoleApplication/Program.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NorthwindConsoleApplication.Logger;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace NorthwindConsoleApplication
 {
@@ -6,7 +15,29 @@ namespace NorthwindConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = CreateDefaultBuilder(args);
+        }
+
+        private static IHost CreateDefaultBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((_, builder) =>
+                {
+                    builder.SetBasePath(Directory.GetCurrentDirectory());
+                    builder.AddJsonFile("appsettings.json", true, true);
+                    builder.Build();
+                    LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddSingleton<ILoggerManager, LoggerManager>();
+                    services.AddLogging(logger =>
+                    {
+                        logger.ClearProviders();
+                        logger.SetMinimumLevel(LogLevel.Information);
+                    });
+                })
+                .Build();
         }
     }
 }
