08b6c45dbad19daf73c93b36b41fc47849fc3920
Josue <josuepereztorr@gmail.com>
2022-05-09 22:09:17 -0500

    Initial Commit

---
+++ NorthwindConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NorthwindConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
